349ca21085d529fe8048e20b637f16b2b1a5ad60
muhammad92 <saadmehmood21@gmail.com>
2014-01-24 04:14:05 -0800

    added map Class named as Map007

---
+++ CrowdSourcePublicToilet/publicToilet/Map007.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publicToilet
+{
+    class Map007
+    {
+        public GeoCoordinate cord { get; set; }
+        public string Town { get; set; }
+
+    }
+}
